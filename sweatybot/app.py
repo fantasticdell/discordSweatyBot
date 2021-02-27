23ddd717808e22c037a8bd6cf61399df3413e070
fantasticdell <79128438+fantasticdell@users.noreply.github.com>
2021-02-27 16:03:27 +1300

    Update app.py
    defined a bot using discord.py

--- sweatybot/app.py
+++ sweatybot/app.py
@@ -11,10 +11,20 @@ from sweatybot import sweat
 #store config / data structures in config.py
 #store application functions in sweat.py
 
-load_dotenv()
+# Set Discord API token
+# this is stored in .env as SWEATY_BOT_DISCORD_TOKEN and should be set before any build
 token = os.getenv("SWEATY_BOT_DISCORD_TOKEN")
 
 if token is None:
     raise RuntimeError("SWEATY_BOT_DISCORD_TOKEN environment variable not set")
+#end Set Discord API token
 
-client.run(token)
+
+
+bot = commands.Bot(command_prefix="!sweaty")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("Test")
+
+bot.run(token)

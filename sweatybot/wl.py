9eed1b29aa66091fb1f150d1c1b88a3da5ed135d
fantasticdell <79128438+fantasticdell@users.noreply.github.com>
2021-02-28 08:57:10 +1300

    Update wl.py
    added get method to query api

--- sweatybot/wl.py
+++ sweatybot/wl.py
@@ -1,3 +1,8 @@
+#pip install -U session
+from requests import Session
+#standard module
+from urllib import parse
+
 #define WarcraftLogs client
 class WarcraftLogs(object):
 
@@ -6,6 +11,17 @@ class WarcraftLogs(object):
 
     def __init__(self, api_key):
         self.api_key = api_key
+        self.session = Session()
+
+#_get will return content from warcraftlogs, when provided with the url path parameters
+    def _get(self, path, **kwargs):
+        params = {"api_key": self.api_key}
+        params.update(kwargs)
+
+        url = parse.urljoin(self.BASE_URL,path)
+
+        return self.session.get(url,params=params)
+
 
     def logtestmessage(self):
         message = "This is an initiated WarcraftLogs object \n base_url is ",self.BASE_URL
